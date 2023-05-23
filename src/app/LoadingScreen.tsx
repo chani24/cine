071c74ae652add96ff5f591250fcbea1ea192e81
unknown <achidera20@gmail.com>
2023-05-23 15:24:27 +0100

    start

--- src/app/LoadingScreen.tsx
+++ src/app/LoadingScreen.tsx
@@ -1,80 +1,106 @@
 'use client';
 
 import Image from 'next/image';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import gsap from "gsap";
 import Typewriter from 'typewriter-effect';
 import styles from "./LoadingScreen.module.css";
 
 export default function LoadingScreen() {
-    const [imageUrl, setImageUrl] = useState("/camera.png");
-    const [startTyping, setStartTyping] = useState(false);
-    const glitchImages = [
-        "/camera-glitch.png",
-        "/camera-glitch-2.png",
-        "/camera-glitch-3.png",
-        "/camera-glitch-4.png",
-    ];
+  const [imageUrl, setImageUrl] = useState("/camera.png");
+  const [percentagePosition, setPercentagePosition] = useState(0)
 
     const glitchImage = () => {
-      const interval =  setInterval(() => {
-            // Generate a random index within the array length
-            const randomIndex = Math.floor(Math.random() * glitchImages.length);
-        
-            // Retrieve the random element using the random index
-            const randomElement = glitchImages[randomIndex];
-        
-            // Do something with the random element (e.g., display it)
-            setImageUrl(randomElement);
-          }, 350);
-
+    setImageUrl('/jittery-camera.gif');
         
        setTimeout(() => {
            setImageUrl("/camera.png");
-           clearInterval(interval);
-           setStartTyping(true);
-      }, 6002)  
+      }, 10000)  
     }
 
     const percentages = [
         '0', '12','20','36','44',
         '52', '64','76','84', '96', '100'
-      ];
+  ];
+  
+  useEffect(() => {
+    gsap.to('.percentage__container', {
+      top: "-" + percentagePosition + "%",
+      delay: 0,
+      duration: 1,
+      ease: "power4.inOut",
+    })
+  },
+    [percentagePosition])
+  
+  const updateScroll = () => {
+    setPercentagePosition(percentagePosition => percentagePosition + 100);
+    }
+  const scrollPercentages = () => {
+    updateScroll();
+    const interval = setInterval(
+      updateScroll
+    , 1000);
+
+    setTimeout(() => {
+      clearInterval(interval);
+    }, 9999)
+  }
 
     useEffect(() => {
         glitchImage();
         gsap.to('.' + styles.progress__bar, {
           width: "100%",
           delay: 0,
-          duration: 6,
-          ease: "power2.inOut",
+          duration: 10,
+          ease: "power0",
         });
-        gsap.to('.percentage__container', {
-          top: "-1000%",
-          delay: 0,
-          duration: 6,
-          ease: "power2.inOut",
-        });
+      scrollPercentages();
+
         gsap.to('.upper_section', {
             opacity: "0",
-            delay: 25,
-            duration: 2,
+            delay: 13.5,
+            duration: 0.5,
             ease: "power2.inOut",
           });
-        gsap.to('.' + styles.cine_logo, {
-            opacity: "1",
-            delay: 25,
-            duration: 2,
+        gsap.to('.' + styles.cine_logo_text, {
+            top: "0",
+            delay: 12,
+            duration: 1,
             ease: "power2.inOut",
-          });
+        });
+        gsap.to('.percentage__container', {
+          top: "-1100%",
+          delay: 12,
+          duration: 1,
+          ease: "power4.inOut",
+        })
+        gsap.to('.percentage_sign__container', {
+          top: "-100%",
+          delay: 12,
+          duration: 1,
+          ease: "power4.inOut",
+        })
         gsap.to('.' + styles.cine_logo, {
+          height: '124px',
+            delay: 14,
+            duration: 0.1,
+            ease: "power0",
+          });
+          gsap.to('.' + styles.cine_logo, {
           bottom: "50%",
           transform: 'translateY(50%)',
-          fontSize: "120px",
-            delay: 28,
-            duration: 2,
+          fontSize: "5rem",
+            delay: 15,
+            duration: 1,
             ease: "power2.inOut",
           });
+          gsap.to('.' + styles.cine_logo_text, {
+            top: "-100%",
+            delay: 17,
+            duration: 1,
+            ease: "power2.inOut",
+        });
       }, []);
 
   return (
@@ -85,9 +111,9 @@ export default function LoadingScreen() {
                   <Typewriter
   options={{
     strings: `"I'll tell you what I'm blathering about... I've got information man! New stuff has come to light! And shit, man, she kidnapped herself. Well sure, man. Look at it... a young trophy wife, in the parlance of our times." - from the movie, The Big Lebowski (1998).`,
-    autoStart: startTyping,
+    autoStart: true,
                               cursor: "",
-                              delay: 50
+                              delay: 3
   }}
 />
   </h1>
@@ -108,10 +134,15 @@ export default function LoadingScreen() {
                   <div className={styles.percentage_wrapper}>
                       <div className="percentage__container relative">{percentages.map((percentage, index) => {
                       return(<div key={index}>{percentage}</div>)
-                  })}</div><div>%</div></div>
+                      })}</div>
+            <div className="percentage_sign__container relative">{' '}%</div>
+                  </div>
         </div>
-       <div className={styles.cine_logo}>
-        CIN/E
+        <div className={styles.cine_logo}>
+          <div className={styles.cine_logo_text}>
+            CIN/E
+          </div>
+        
         </div>
       </div>
        
